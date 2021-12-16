700e8afc7e31b1316be657ac9091930fc928ef16
David Wadmark <david.wadmark@gmail.com>
2021-12-16 21:59:00 +0100

    Solve 15.1

--- 2021/15/shitton.cpp
+++ 2021/15/shitton.cpp
@@ -6,9 +6,29 @@
 #include <queue>
 #include <vector>
 #include <set>
+#include <map>
 #include <chrono>
 #include "graph.h"
 using namespace std;
+
+
+class VertexCmp {
+    public:
+        bool operator() (const Vertex v1, const Vertex v2) const {
+            return (v1.d > v2.d);
+        }
+};
+
+class VertexMapCmp {
+    public:
+        bool operator() (const Vertex& v1, const Vertex& v2) const {
+            return (v1.d > v2.d);
+        }
+};
+
+bool mapcomp (const Vertex v1, const Vertex v2) {
+    return (v1.d > v2.d);
+}
 
 bool inside_grid(int i, int j, int gridsize) {
     return i >= 0 && j >= 0 && i < gridsize && j < gridsize;
@@ -21,12 +41,19 @@ bool exists(Vertex vertex, list<Vertex> vertices) {
     return false;
 }
 
-bool valid(Vertex v, list<pair<int,Vertex>> used) {
-    for (pair<int,Vertex> p : used) {
-        if (p.first == v.d && p.second == v) { return false; }
-    }
-    return true;
-}
+// bool valid(Vertex v, map<Vertex,int,bool(*)(Vertex,Vertex)> min) {
+//     int minval = min[v];
+//     if (minval < v.d) { return false; }
+//     return true;
+// }
+
+// void update_min(Vertex v, map<Vertex,int,bool(*)(Vertex,Vertex)>* min) {
+//     auto p = min->insert(pair<Vertex,int>(v, v.d));
+//     if (!p.second) {
+//         //*p.first = pair<Vertex,int>(v,v.d);
+//         p.first->second = v.d;
+//     }
+// }
 
 void print_vertex(Vertex v) {
     cout << v.iloc << ',' << v.jloc << ',' << v.d << '\n';
@@ -43,20 +70,15 @@ void print_graph(Graph G) {
     }
 }
 
-class VertexCmp {
-    public:
-        bool operator() (const Vertex v1, const Vertex v2) const {
-            return (v1.d > v2.d);
-        }
-};
 
-list<Vertex> dijkstra(Graph G, Vertex s) {
-    list<Vertex> V = G.vertices;
-    list<Vertex> S;
+
+set<Vertex> dijkstra(Graph G, Vertex s, Vertex t) {
+    set<Vertex> S;
     priority_queue<Vertex,vector<Vertex>,VertexCmp> Q;
-    list<pair<int,Vertex>> used;
+    bool(*fn_pt)(Vertex,Vertex) = mapcomp;
+    map<Vertex,int,bool(*)(Vertex,Vertex)> min (fn_pt);
 
-    for (Vertex v : V) {
+    for (Vertex v : G.vertices) {
         if (v == s) {
             v.d = 0;
         }
@@ -66,21 +88,27 @@ list<Vertex> dijkstra(Graph G, Vertex s) {
     while (!Q.empty()) {
         cout << n << '\n';
         Vertex u = Q.top();
+        if (u == t) { break; }
         Q.pop();
-        while (!valid(u, used) && !Q.empty()) {
+        while (!valid(u, min) && !Q.empty()) {
             u = Q.top();
             Q.pop();
         }
-        S.push_back(u);
+        S.insert(u);
         for (Edge e : G.neighb_edges(u)) {
             Vertex* v = e.end;
             int w = e.weight;
-            if (!exists(*v, S)) {
+            if (S.find(*v) != S.end()) {
                 if (u.d + w < v->d) {
-                    used.push_back(pair<int,Vertex>{v->d, *v});
                     v->d = u.d + w;
                     v->pred = &u;
                     Q.push(*v);
+                    update_min(*v, &min);
+                    // auto p = min->insert(pair<Vertex,int>(v, v.d));
+                    // if (!p.second) {
+                    //     //*p.first = pair<Vertex,int>(v,v.d);
+                    //     p.first->second = v.d;
+                    // }
                 }
             }
 
@@ -130,7 +158,7 @@ int main() {
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    list<Vertex> S = dijkstra(G, vertices.at(0).at(0));
+    set<Vertex> S = dijkstra(G, vertices.at(0).at(0), vertices.at(gridsize-1).at(gridsize-1));
 
     Vertex dest;
     for (Vertex v : S) {
