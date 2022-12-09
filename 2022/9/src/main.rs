7e5f4edfa89b313483e355793b6893cefc719443
cubwadmark <david.wadmark@cubist.eu>
2022-12-09 08:48:04 +0100

    Solve part one of day 9

---
+++ 2022/9/src/main.rs
@@ -0,0 +1,8 @@
+mod ropes1;
+mod ropes2;
+
+fn main() {
+    ropes1::ropes_1();
+    // ropes2::ropes_2();
+}
+
---
+++ 2022/9/src/ropes1.rs
@@ -0,0 +1,53 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::{BufRead, BufReader};
+
+pub fn ropes_1() {
+    let buff_reader = BufReader::new(fs::File::open("input.txt").expect("Failed to open file!"));
+    let mut n_visited: u32 = 0;
+    let mut visited: HashMap<(i32, i32), bool> = HashMap::new();
+    let mut head_coords = (0, 0);
+    let mut tail_coords = (0, 0);
+
+    for line in buff_reader.lines() {
+        let parsed: String = line.expect("Failed to parse line!").trim().parse().expect("Failed to parse line!");
+        let mut split = parsed.split_whitespace();
+        let direction: char = split.next().expect("Iterator out of bounds!").chars().next().expect("Failed to parse char!");
+        let steps: i32 = split.next().expect("Iterator out of bounds!").parse().expect("Failed to parse string as u32!");
+
+        for _ in 0..steps {
+            match direction {
+                'U' => head_coords.1 += 1,
+                'R' => head_coords.0 += 1,
+                'D' => head_coords.1 -= 1,
+                'L' => head_coords.0 -= 1,
+                _ => panic!("Unexpected char!")
+            }
+            tail_coords = calc_tail_coords(head_coords, tail_coords);
+            visited.insert(tail_coords, true);
+            // println!("Head coords: ({0}, {1})", head_coords.0, head_coords.1);
+            // println!("Tail coords: ({0}, {1})", tail_coords.0, tail_coords.1);
+            // println!("---------------------------------------");
+        }
+
+    }
+
+    println!("{}", visited.len());
+}
+
+fn calc_tail_coords(head_coords: (i32, i32), tail_coords: (i32, i32)) -> (i32, i32) {
+    let dx: i32 = head_coords.0 - tail_coords.0;
+    let dy: i32 = head_coords.1 - tail_coords.1;
+    
+    if dx.abs() > 1 && dy == 0 { // Horizontally straight
+        return (tail_coords.0 + dx / 2, tail_coords.1);
+    } else if dx == 0 && dy.abs() > 1 { // Vertically straight
+        return (tail_coords.0, tail_coords.1 + dy / 2);
+    } else if dx.abs() > 1 && dy.abs() == 1 { // Horizontally diagonal
+        return (tail_coords.0 + dx / 2, tail_coords.1 + dy);
+    } else if dx.abs() == 1 && dy.abs() > 1 { // Vertically diagonal
+        return (tail_coords.0 + dx, tail_coords.1 + dy / 2);
+    } else { // Don't need to move
+        return tail_coords;
+    }
+}
