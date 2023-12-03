f46248cf5ae82e6b4cc94c481a84c1d6f87d5387
David Wadmark <david.wadmark@gmail.com>
2023-12-03 16:44:15 +0100

    Solve day 3

---
+++ 2023/3/gondola1.py
@@ -0,0 +1,48 @@
+import numpy as np
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class SchematicNumber:
+    value: int
+    indexes: list[tuple[int, int]]
+
+def has_adjacent_symbol(indexes: list[tuple[int, int]], schematic: np.ndarray):
+    for index in indexes:
+        y = index[0]
+        x = index[1]
+        for i in [max(y - 1, 0), y, min(y + 1, schematic.shape[0] - 1)]:
+            for j in [max(x - 1, 0), x, min(x + 1, schematic.shape[1] - 1)]:
+                if schematic[i,j] == -1:
+                    return True
+    return False
+
+
+sum = 0
+
+file = open('input.txt', 'r')
+lines = [line.strip() for line in file.readlines()]
+
+schematic = np.zeros((len(lines), len(lines[0])))
+schematic_numbers: list[SchematicNumber] = []
+
+for i, line in enumerate(lines):
+    matches = re.finditer(r'\d+', line)
+    for match in matches:
+        span = match.span()
+        indexes = [(i, j) for j in range(span[0], span[1])]
+        number = SchematicNumber(value=int(match.group(0)), indexes=indexes)
+        schematic_numbers.append(number)
+
+    for j, char in enumerate(line):
+        if char.isdigit():
+            schematic[i,j] = int(char)
+        elif not char == '.':
+            schematic[i,j] = -1
+
+for number in schematic_numbers:
+    if has_adjacent_symbol(number.indexes, schematic):
+        sum += number.value
+
+print(sum)
---
+++ 2023/3/gondola2.py
@@ -0,0 +1,59 @@
+import numpy as np
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class SchematicNumber:
+    value: int
+    indexes: list[tuple[int, int]]
+
+def adjacent_numbers(gear: tuple[int, int], numbers: list[SchematicNumber]) -> list[int]:
+    adjacent_numbers = set()
+    y = gear[0]
+    x = gear[1]
+
+    for i in [max(y - 1, 0), y, min(y + 1, schematic.shape[0] - 1)]:
+        for j in [max(x - 1, 0), x, min(x + 1, schematic.shape[1] - 1)]:
+            for number in numbers:
+                if is_adjacent((i, j), number):
+                    adjacent_numbers.add(number.value)
+
+    return list(adjacent_numbers)
+
+def is_adjacent(gear_index: tuple[int, int], number: SchematicNumber):
+    for index in number.indexes:
+        if gear_index == index:
+            return True
+    return False
+
+
+sum = 0
+
+file = open('input.txt', 'r')
+lines = [line.strip() for line in file.readlines()]
+
+schematic = np.zeros((len(lines), len(lines[0])))
+schematic_numbers: list[SchematicNumber] = []
+gears = []
+
+for i, line in enumerate(lines):
+    matches = re.finditer(r'\d+', line)
+    for match in matches:
+        span = match.span()
+        indexes = [(i, j) for j in range(span[0], span[1])]
+        number = SchematicNumber(value=int(match.group(0)), indexes=indexes)
+        schematic_numbers.append(number)
+
+    for j, char in enumerate(line):
+        if char.isdigit():
+            schematic[i,j] = int(char)
+        elif char == '*':
+            gears.append((i, j))
+
+for gear in gears:
+    adjacent_nums = adjacent_numbers(gear, schematic_numbers)
+    if len(adjacent_nums) == 2:
+        sum += adjacent_nums[0] * adjacent_nums[1]
+
+print(sum)
