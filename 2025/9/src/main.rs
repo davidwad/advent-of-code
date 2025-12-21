96d6b9f4cc3ef008caac02f3be4ad9217f3b1e03
David Wadmark <david.wadmark@cubist.eu>
2025-12-21 14:55:21 +0100

    Solve day 9 part 2

--- 2025/9/src/main.rs
+++ 2025/9/src/main.rs
@@ -1,7 +1,22 @@
+use std::thread;
+
 mod part1;
 mod part2;
 
-fn main() {
+const STACK_SIZE: usize = 256 * 1024 * 1024;
+
+fn run() {
     part1::part1();
     part2::part2();
+}
+
+fn main() {
+    // Spawn thread with explicit stack size
+    let child = thread::Builder::new()
+        .stack_size(STACK_SIZE)
+        .spawn(run)
+        .unwrap();
+
+    // Wait for thread to join
+    child.join().unwrap();
 }
--- 2025/9/src/part2.rs
+++ 2025/9/src/part2.rs
@@ -1,66 +1,78 @@
-use core::panic;
 use std::collections::HashSet;
 use std::fs;
 use std::io::{BufRead, BufReader};
-
-// NEW IDEA: Fill matrix of outside tiles by expanding from a tile outside of the inner tiles, e.g. (0,0)
-// TODO: 422041553 and 1554335760 was too low. Probably need to handle some special case, maybe
-// ..............
-// .#XXXXX#XXX#..
-// .#.........X..
-// .#XXX#.....X..
-// ..#XX#.....X..
-// ..#XXXXXX#.X..
-// .........X.X..
-// .........#X#..
-// ..............
+use std::panic;
 
 pub fn part2() {
-    let clockwise = true;
-    let mut tiles = Vec::<(i64, i64)>::new();
-    let mut mines = Vec::<(i64, i64)>::new();
-    let mut buff_reader = BufReader::new(fs::File::open("input.txt").expect("Failed to read input!"));
-    let mut previous_tile = (0 as i64, 0 as i64);
+    let mut corners = Vec::<(i64, i64)>::new();
+    let mut walls = HashSet::<(i64, i64)>::new();
+
+    let mut buff_reader =
+        BufReader::new(fs::File::open("input.txt").expect("Failed to read input!"));
+    let mut prev_corner = (0 as i64, 0 as i64);
     let mut i = 0;
     loop {
         let mut line = String::new();
-        let num_bytes = buff_reader.read_line(&mut line).expect("Failed to read input!");
+        let num_bytes = buff_reader
+            .read_line(&mut line)
+            .expect("Failed to read input!");
         if num_bytes == 0 {
             break;
         }
 
         let split: Vec<&str> = line.trim().split(",").collect();
-        let tile = (split[0].parse::<i64>().expect("Failed to parse coordinate as i32"), split[1].parse::<i64>().expect("Failed to parse coordinate as i32"));
-        tiles.push(tile);
+        let corner = (
+            split[0]
+                .parse::<i64>()
+                .expect("Failed to parse coordinate as i32"),
+            split[1]
+                .parse::<i64>()
+                .expect("Failed to parse coordinate as i32"),
+        );
+        corners.push(corner);
 
         if i > 0 {
-            let mine_coordinates = get_mine_coordinates(previous_tile, tile, clockwise);
-            for c in mine_coordinates {
-                mines.push(c);
-            }
+            build_wall(&mut walls, prev_corner, corner);
         }
-        previous_tile = tile;
-        i += 1;
 
-        if (tile.0 - previous_tile.0).abs() == 1 || (tile.1 - previous_tile.1).abs() == 1 {
-            println!("Special case: ({},{}) - ({},{})", previous_tile.0, previous_tile.1, tile.0, tile.1);
-        }
+        prev_corner = corner;
+        i += 1;
     }
 
-    let mine_coordinates = get_mine_coordinates(tiles[tiles.len() - 1], tiles[0], clockwise);
-    for c in mine_coordinates {
-        mines.push(c);
+    build_wall(&mut walls, corners[corners.len() - 1], corners[0]);
+
+    let start_pos1: (i64, i64);
+    let start_pos2: (i64, i64);
+    let x_diff = corners[0].0 - corners[1].0;
+    if x_diff == 0 {
+        // Vertical start
+        start_pos1 = (corners[0].0 - 1, corners[0].1);
+        start_pos2 = (corners[0].0 + 1, corners[0].1);
+    } else {
+        // Horizontal start
+        start_pos1 = (corners[0].0, corners[0].1 - 1);
+        start_pos2 = (corners[0].0, corners[0].1 + 1);
+    }
+
+    let outside: HashSet<(i64, i64)>;
+    let mut outside1 = HashSet::<(i64, i64)>::new();
+    let mut outside2 = HashSet::<(i64, i64)>::new();
+    crawl_along_wall(&walls, &mut outside1, start_pos1);
+    crawl_along_wall(&walls, &mut outside2, start_pos2);
+
+    if outside1.len() > outside2.len() {
+        outside = outside1;
+    } else {
+        outside = outside2;
     }
 
     let mut max_area: i64 = 0;
-    for i in 0..tiles.len() {
-        for j in i+1..tiles.len() {
-            let t1 = tiles[i];
-            let t2 = tiles[j];
-            // if rectangle_contains_tile(&tiles, t1, t2) || rectangle_contains_tile(&mines, t1, t2) {
-            //     continue;
-            // }
-            if rectangle_contains_tile(&mines, t1, t2) {
+    for i in 0..corners.len() {
+        for j in i + 1..corners.len() {
+            let t1 = corners[i];
+            let t2 = corners[j];
+
+            if rectangle_contains_outside(&outside, t1, t2) {
                 continue;
             }
 
@@ -69,76 +81,73 @@ pub fn part2() {
             let area = dx * dy;
             if area > max_area {
                 max_area = area;
-                // println!("({}, {})  ({}, {})", t1.0, t1.1, t2.0, t2.1);
             }
         }
     }
 
-    // print_tiles(&tiles, &mines);
-
     println!("{max_area}")
-
 }
 
-fn get_mine_coordinates(previous: (i64, i64), current: (i64, i64), clockwise: bool) -> HashSet::<(i64, i64)> {
-    let mut res = HashSet::<(i64, i64)>::new();
-    if previous.0 < current.0 { // Going right
-        if clockwise {
-            for x in previous.0..current.0 + 1 {
-                res.insert((x, current.1 - 1));
-            }
-            return res;
-        } else {
-            for x in previous.0..current.0 + 1 {
-                res.insert((x, current.1 + 1));
-            }
-            return res;
+fn build_wall(walls: &mut HashSet<(i64, i64)>, previous: (i64, i64), current: (i64, i64)) {
+    if previous.0 < current.0 {
+        // Going right
+        for x in previous.0..current.0 + 1 {
+            walls.insert((x, current.1));
         }
-    } else if previous.0 > current.0 { // Going left
-        if clockwise {
-            for x in current.0..previous.0 + 1 {
-                res.insert((x, current.1 + 1));
-            }
-            return res;
-        } else {
-            for x in current.0..previous.0 + 1 {
-                res.insert((x, current.1 - 1));
-            }
-            return res;
+    } else if previous.0 > current.0 {
+        // Going left
+        for x in current.0..previous.0 + 1 {
+            walls.insert((x, current.1));
         }
-    } else if previous.1 < current.1 { // Going down
-        if clockwise {
-            for y in previous.1..current.1 + 1 {
-                res.insert((current.0 + 1, y));
-            }
-            return res;
-        } else {
-            for y in previous.1..current.1 + 1 {
-                res.insert((current.0 - 1, y));
-            }
-            return res;
+    } else if previous.1 < current.1 {
+        // Going down
+        for y in previous.1..current.1 + 1 {
+            walls.insert((current.0, y));
         }
-    } else if previous.1 > current.1 { // Going up
-        if clockwise {
-            for y in current.1..previous.1 + 1 {
-                res.insert((current.0 - 1, y));
-            }
-            return res;
-        } else {
-            for y in current.1..previous.1 + 1 {
-                res.insert((current.0 + 1, y));
-            }
-            return res;
+    } else if previous.1 > current.1 {
+        // Going up
+        for y in current.1..previous.1 + 1 {
+            walls.insert((current.0, y));
         }
     } else {
         panic!("Failed to calculate direction");
     }
 }
 
-fn rectangle_contains_tile(tiles: &Vec::<(i64, i64)>, t1: (i64, i64), t2: (i64, i64)) -> bool {
-    for tile in tiles {
-        if is_tile_inside_rectangle(*tile, t1, t2) {
-            // println!("({}, {})  ({}, {})", t1.0, t1.1, t2.0, t2.1);
+fn crawl_along_wall(
+    walls: &HashSet<(i64, i64)>,
+    outside: &mut HashSet<(i64, i64)>,
+    pos: (i64, i64),
+) {
+    if walls.contains(&pos) || outside.contains(&pos) {
+        return;
+    }
+
+    let mut wall_adjacent = false;
+    for x in pos.0 - 1..pos.0 + 2 {
+        for y in pos.1 - 1..pos.1 + 2 {
+            if walls.contains(&(x, y)) {
+                wall_adjacent = true;
+            }
+        }
+    }
+
+    if wall_adjacent {
+        outside.insert(pos);
+        crawl_along_wall(walls, outside, (pos.0 + 1, pos.1));
+        crawl_along_wall(walls, outside, (pos.0 - 1, pos.1));
+        crawl_along_wall(walls, outside, (pos.0, pos.1 + 1));
+        crawl_along_wall(walls, outside, (pos.0, pos.1 - 1));
+    }
+}
+
+fn rectangle_contains_outside(
+    outside: &HashSet<(i64, i64)>,
+    t1: (i64, i64),
+    t2: (i64, i64),
+) -> bool {
+    for tile in outside {
+        if rectangle_contains_tile(*tile, t1, t2) {
             return true;
         }
     }
@@ -146,7 +155,7 @@ fn rectangle_contains_tile(tiles: &Vec::<(i64, i64)>, t1: (i64, i64), t2: (i64, 
     return false;
 }
 
-fn is_tile_inside_rectangle(tile: (i64, i64), t1: (i64, i64), t2: (i64, i64)) -> bool {
+fn rectangle_contains_tile(tile: (i64, i64), t1: (i64, i64), t2: (i64, i64)) -> bool {
     if tile == t1 || tile == t2 {
         return false;
     }
@@ -160,9 +169,6 @@ fn is_tile_inside_rectangle(tile: (i64, i64), t1: (i64, i64), t2: (i64, i64)) ->
             return false;
         }
     } else {
-        // if tile.0 != t1.0 {
-        //     return false;
-        // }
         return false;
     }
 
@@ -175,43 +181,26 @@ fn is_tile_inside_rectangle(tile: (i64, i64), t1: (i64, i64), t2: (i64, i64)) ->
             return false;
         }
     } else {
-        // if tile.1 != t1.1 {
-        //     return false;
-        // }
         return false;
     }
 
     return true;
 }
 
-fn print_tiles(tiles: &Vec::<(i64, i64)>, mines: &Vec::<(i64, i64)>) {
-    let mut min_x = i64::MAX;
-    let mut min_y = i64::MAX;
-    let mut max_x = i64::MIN;
-    let mut max_y = i64::MIN;
-    for t in tiles {
-        if t.0 < min_x {
-            min_x = t.0;
-        }
-        if t.1 < min_y {
-            min_y = t.1;
-        }
-        if t.0 >  max_x {
-            max_x = t.0;
-        }
-        if t.1 > max_y {
-            max_y = t.1;
-        }
-    }
-
-    for y in min_y-1..max_y+2 {
-        for x in min_x-2..max_x+3 {
-            assert!(!(tiles.contains(&(x, y)) && mines.contains(&(x, y))));
-
-            if tiles.contains(&(x, y)) {
+fn print_tiles(
+    corners: &Vec<(i64, i64)>,
+    walls: &HashSet<(i64, i64)>,
+    outside: &HashSet<(i64, i64)>,
+) {
+    for y in 0..9 {
+        for x in 0..13 {
+            if corners.contains(&(x, y)) {
+                assert!(walls.contains(&(x, y)));
                 print!("#");
-            } else if mines.contains(&(x, y)) {
+            } else if walls.contains(&(x, y)) {
                 print!("X");
+            } else if outside.contains(&(x, y)) {
+                print!("O");
             } else {
                 print!(".");
             }
